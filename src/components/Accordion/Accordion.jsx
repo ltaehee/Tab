197582762bb068b62a83bdd20126a8ec8a3adce7
이태희 <taehee3992@naver.com>
2024-10-29 17:51:03 +0900

    Tab, Accordion 완료

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,29 +3,35 @@ import AccordionButton from "./AccordionButton"
 import AccordionContent from "./AccordionContent";
 
 const Accordion = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndexList, setActiveIndexList] = useState([]);
 
   const onClickAccordionButton = (index) => {
-    console.log(index);
-    setActiveIndex(index);
+    // 기존 배열에 index가 있는 경우
+    if (activeIndexList.includes(index)) {
+      setActiveIndexList((prev) => prev.filter((prevIndex) => prevIndex !== index));
+    }
+    // ~~ 없는 경우
+    else {
+      setActiveIndexList((prev) => [...prev, index]);
+    }
   };
   
   return (
     <>
       <AccordionButton title={"ACC-1"} index={0} onClickAccordionButton={onClickAccordionButton} />
-      <AccordionContent isActive={activeIndex === 0}>
+      <AccordionContent isActive={activeIndexList.includes(0)}>
         <div>
           <h3>Accordion-Content-1</h3>
         </div>
       </AccordionContent>
       <AccordionButton title={"ACC-2"} index={1} onClickAccordionButton={onClickAccordionButton} />
-      <AccordionContent isActive={activeIndex === 1}>
+      <AccordionContent isActive={activeIndexList.includes(1)}>
         <div>
           <h3>Accordion-Content-2</h3>
         </div>
       </AccordionContent>
       <AccordionButton title={"ACC-3"} index={2} onClickAccordionButton={onClickAccordionButton} />
-      <AccordionContent isActive={activeIndex === 2}>
+      <AccordionContent isActive={activeIndexList.includes(2)}>
         <div>
           <h3>Accordion-Content-3</h3>
         </div>
